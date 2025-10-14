ec1249aba83a9748bfc04c57a641a54f04fcb68f
ChuchoCoder <traderchucho@gmail.com>
2025-10-14 10:19:32 -0300

    feat: replace BrowserRouter with HashRouter and ensure public directory is copied to dist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './app/App.jsx';
@@ -8,10 +8,10 @@ import { ConfigProvider } from './state/config-context.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ConfigProvider>
         <App />
       </ConfigProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
